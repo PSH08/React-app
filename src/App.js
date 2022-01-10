9acda5765c479c423cfadde7328479ed2a94e063
lolu1023 <soowkw@naver.com>
2022-01-10 21:31:46 +0900

    5일차 to do list part

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
     setToDos(currentArray => [toDo, ...currentArray])
     setToDo("");
   };
+  console.log(toDos)
   return (
     <div>
       <h1>My To Dos ({toDos.length})</h1>
@@ -19,6 +20,10 @@ function App() {
         <input onChange={onChange} value={toDo} type="text" placeholder='Write your to do....' />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item,index) => <li key={index}>{item}</li>)}
+      </ul>
     </div>
   );
 }
