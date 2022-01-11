abd2442b5a22f2793eafb7290222f3fe108033ef
lolu1023 <soowkw@naver.com>
2022-01-11 22:34:26 +0900

    6일차 9:12초

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import { useEffect } from 'react/cjs/react.development';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([])
+  const [movies, setMovies] = useState([])
+  const getMovies = async() => {
+    const json = await(
+      await fetch(
+        'https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year'
+      )
+    ).json();
+    setMovies(json.data.movies)
+    setLoading(false)
+  }
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {
-      setCoins(json);
-      setLoading(false);
-    })
+    getMovies()
   },[])
   return (
     <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : 
-      <select>
-        {coins.map((coin) => <option>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price}</option>)}
-      </select>}
+      {loading ? <h1>Loading....</h1> : null}
     </div>
   );
 }
