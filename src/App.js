f99f054fed75f83da5378713ccdb83f4a1613f7e
lolu1023 <soowkw@naver.com>
2022-01-18 22:56:32 +0900

    11일차

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
   return (
   <Router>
     <Routes>
-      <Route path="/movie/:id" element={<Detail />}></Route>
-      <Route path={`${process.env.PUBLIC_URL}/`} element={Home}></Route>
+      <Route path="/movie/:id" element={<Detail />} />
+      <Route path="/" element={<Home />} />
     </Routes>
   </Router>
   )
